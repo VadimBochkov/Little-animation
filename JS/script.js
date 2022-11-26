021c53d62fff3201bdd13bf25c27a4b3c1221ae6
Vadim Bochkov <vadim.bochkov.96@gmail.com>
2022-11-26 22:15:24 +0600

    Edit file

--- JS/script.js
+++ JS/script.js
@@ -7,12 +7,13 @@ let macBook = document.querySelector('.header__img'),
 function moveLeeter() {
   let sizeLetter = 82,
       sizeImg = 1100,
-      opacity = 0;
+      opacity = 8;
 
-  let id = setInterval(frame, 25);
+  let id = setInterval(frame, 25),
+      id2 = setInterval(mac, 25);
 
   function frame() {
-    sizeImg-- ;
+    sizeImg--;
     sizeLetter++;
     opacity++;
 
@@ -22,10 +23,19 @@ function moveLeeter() {
 
     text.style.opacity = opacity + '%';
     macBook.style.width = sizeImg + 'px';
-
     
-    if (sizeImg == 950 || sizeLetter == 155) {
+    if (sizeLetter == 155) {
       clearInterval(id);
+    }
+  }
+
+  function mac() {
+    sizeImg--;
+
+    macBook.style.width = sizeImg + 'px';
+    
+    if (sizeImg == 950) {
+      clearInterval(id2);
     }
   }
 }
